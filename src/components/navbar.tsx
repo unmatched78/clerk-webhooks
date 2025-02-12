10bdf71633b41708f9068d0fdd0ce05388be7d3b
candraKriswinarto <ckriswinarto@gmail.com>
2025-02-12 09:40:23 +0800

    first commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,29 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-white shadow-lg">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-between items-center h-16">
+          <Link href="/" className="text-xl font-bold">
+            ShopEase
+          </Link>
+
+          <div className="flex items-center space-x-4">
+            <SignedOut>
+              <SignInButton>
+                <button className="px-4 py-2 rounded-md border border-black bg-white text-black text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200">
+                  Sign in
+                </button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
